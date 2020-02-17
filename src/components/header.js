b9cfaccd5b06461afbf20c3db031bb1ef49b8ad7
Ari Perez <ariperez83@gmail.com>
2020-02-16 18:39:22 -0600

    Working Navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,36 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar"
+import Image from "react-bootstrap/Image"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <Navbar bg="light" expand="lg">
+    <Image
+      style={{ maxWidth: "200px" }}
+      class={"d-lg-none"}
+      src="/embodhii-logo.png"
+      fluid
+    />
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">About</Nav.Link>
+        <Nav.Link href="#link">Services</Nav.Link>
+        <Image
+          style={{ maxWidth: "300px" }}
+          class={"d-none d-lg-block "}
+          src="/embodhii-logo.png"
+          fluid
+        />
+        <Nav.Link href="#link">Blog</Nav.Link>
+        <Nav.Link href="#link">FAQ</Nav.Link>
+        <Nav.Link href="#link">Contact</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
