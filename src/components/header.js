3564a554ac999870619c0c027f4e713038c76eab
Ari Perez <ariperez83@gmail.com>
2020-02-17 15:04:21 -0600

    Removing CSS frameworks; going it alone

--- src/components/header.js
+++ src/components/header.js
@@ -1,37 +1,15 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
-import Nav from "react-bootstrap/Nav"
-import Navbar from "react-bootstrap/Navbar"
-import Image from "react-bootstrap/Image"
+import { Link } from "gatsby"
 
 const Header = ({ siteTitle }) => (
-  <Navbar bg="light" expand="lg">
-    <Image
-      style={{ maxWidth: "200px" }}
-      class={"d-lg-none d-xl-none "}
+  <>
+    <img
+      style={{ maxWidth: "300px" }}
+      class={"d-xs-none d-sm-none d-md-none"}
       src="/embodhii-logo.png"
       fluid
     />
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link href="/">Home</Nav.Link>
-        <Nav.Link href="/about/">About</Nav.Link>
-        <Nav.Link href="/services/">Services</Nav.Link>
-
-        <Image
-          style={{ maxWidth: "300px" }}
-          class={"d-xs-none d-sm-none d-md-none"}
-          src="/embodhii-logo.png"
-          fluid
-        />
-        <Nav.Link href="/blog/">Blog</Nav.Link>
-        <Nav.Link href="/faq/">FAQ</Nav.Link>
-        <Nav.Link href="/contact/">Contact</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+  </>
 )
 
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -9,9 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import "bootstrap/dist/css/bootstrap.min.css"
 import Header from "./header"
-import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
